32d6313fa0d62dd30c6b98b3042e66166ec7a886
SameerCD30 <sameer.cd30@gmail.com>
2026-04-08 01:16:08 +0530

    started Building backend for an ecom webapp

---
+++ src/main/java/com/sameer/Ecom/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sameer.Ecom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Ecom Test ran successfully";
+    }
+}
---
+++ src/test/java/com/sameer/Ecom/EcomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.Ecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
