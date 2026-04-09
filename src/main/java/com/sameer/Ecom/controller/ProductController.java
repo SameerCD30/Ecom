d2d65245692553acca4e610ea791d45969af4e20
SameerCD30 <sameer.cd30@gmail.com>
2026-04-10 02:25:13 +0530

    Handling Image in Backend, but leaving with a bug. Will solve it tomorrow

--- src/main/java/com/sameer/Ecom/controller/ProductController.java
+++ src/main/java/com/sameer/Ecom/controller/ProductController.java
@@ -1,16 +1,53 @@
 package com.sameer.Ecom.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sameer.Ecom.model.Product;
+import com.sameer.Ecom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
-    public String getProducts(){
-        return "All Products";
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(productService.getAllProduct(), HttpStatus.OK);
     }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product != null){
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+     
 
 }
--- src/main/java/com/sameer/Ecom/model/Product.java
+++ src/main/java/com/sameer/Ecom/model/Product.java
@@ -1,9 +1,7 @@
 package com.sameer.Ecom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,10 +22,20 @@ public class Product {
     private String name;
     private String description;
     private String brand;
+
     private BigDecimal price;
     private String category;
-    private Date releaseDate;
-    private boolean productAvailable;
-    private int stockQuantity;
 
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date releaseDate;
+
+    private boolean productAvailable;
+
+    private Integer stockQuantity;
+
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
 }
